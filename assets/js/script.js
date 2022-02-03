4dd0730762c4ee74b866cc7d01ef40dcf1dc3d4b
Caroline Konrad <ckonrad@live.unc.edu>
2022-02-02 20:21:04 -0500

    Making head/tail flipper work

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,22 +6,53 @@ document.addEventListener('DOMContentLoaded', function () {
     // You can remove it once you see it in your browser console in the developer tools
     console.log('Hi')
 
-    // TODO: Add event listener and handler for flip and clear buttons
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
+})
+let numHeads = document.querySelector('#heads-num')
+let percHeads = document.querySelector('#heads-percent')
+let numTails = document.querySelector('#tails-num')
+let percTails = document.querySelector('#tails-percent')
+let pennyImage = document.querySelector('#penny-image')
+let totalFlips = 0
+let numHeadRolls = 0
+let numTailRolls = 0
 
 
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
+//flip the penny
+let flipPenny = document.querySelector('#flip')
+flipPenny.addEventListener('click', function(){
+    let rolledValue = Math.round(Math.random() * 100)
+    console.log(rolledValue)
 
+if (rolledValue < 50){
+        numHeadRolls += 1
+        numHeads.textContent = numHeadRolls
+        pennyImage.src = "file:///C:/Users/carol/InClassAct/FSI-Implement-Coin-Flipper/assets/images/penny-heads.jpg"
+        pennyImage.alt = "penny head image"
+        totalFlips += 1 
+        percHeads.textContent = Math.round(numHeadRolls / totalFlips * 100) + "%"
+        if (numTailRolls > 0){
+            percTails.textContent = Math.round(numTailRolls / totalFlips * 100) + "%"
+        }
+    } else {
+        numTailRolls += 1
+        numTails.textContent = numTailRolls
+        pennyImage.src = "file:///C:/Users/carol/InClassAct/FSI-Implement-Coin-Flipper/assets/images/penny-heads.jpg"
+        pennyImage.alt = "penny tails image"
+        totalFlips += 1
+        percTails.textContent = Math.round(numTailRolls / totalFlips * 100) + "%"
+        if (numHeadRolls > 0) {
+            percHeads.textContent = Math.round(numHeadRolls / totalFlips * 100) + "%"
+        }
+    }
+})
+
+let clearScore = document.querySelector('#clear')
+clearScore.addEventListener('click', function(){
+    totalFlips = 0
+    numHeadRolls = 0
+    numTailRolls = 0
+    numHeads.textContent = 0
+    numTails.textContent = 0
+    percHeads.textContent = "0%"
+    percTails.textContent = "0%"
 })
